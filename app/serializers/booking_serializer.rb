19f199a2e2761a6e01e87cb92b7807ef41808c6a
ignatius22 <igee54077@gmail.com>
2025-04-12 09:33:02 +0100

    make change to the serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,6 +1,14 @@
 class BookingSerializer
   include JSONAPI::Serializer
-  attributes :id, :start_date, :end_date, :created_at, :updated_at, :total_price, :num_nights, :num_guests, :status
+  attributes :id, :start_date, :end_date, 
+             :created_at, :updated_at, :total_price, 
+             :num_nights, :num_guests, :status,
+             :has_breakfast, :observations,
+             :extras_price,
+             :cabin_price,
+             :is_paid
+
+
   belongs_to :cabin
   belongs_to :user
 end
