02f610482d6527584442715b3dff6ed2c23e8532
ignatius22 <igee54077@gmail.com>
2025-04-12 09:43:08 +0100

    make change to the serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :full_name, :role, :is_authenticated
+  attributes :id, :email, :full_name, :role, 
+             :is_authenticated, :nationality, :country_flag, :national_id
 end
